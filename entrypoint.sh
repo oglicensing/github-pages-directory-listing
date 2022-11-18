e2de765251ba1c8bb6ee1b9b46a083f763f5bb52
i0rt <i.ortega@onneragroup.com>
2022-11-18 15:19:00 +0100

    remove duplicated head

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install cmake python3 -y
 git clone --depth 1 https://github.com/jayanta525/apindex-v2.git
 cd apindex-v2
-sed -i '/<\/head>/i <script defer data-domain="oglicensing.github.io" src="https://plausible.onneragroup.com/js/plausible.js"></script></head>' share/apindex/index.template.html
+sed -i '/<\/head>/i <script defer data-domain="oglicensing.github.io" src="https://plausible.onneragroup.com/js/plausible.js"></script>' share/apindex/index.template.html
 cmake . -DCMAKE_INSTALL_PREFIX=/usr
 sudo make install
 cd ..
